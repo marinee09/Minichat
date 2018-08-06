9220077118c1b46bdf35fbe3825707cc32af801c
marinee09 <40633491+marinee09@users.noreply.github.com>
2018-08-06 16:17:15 +0200

    Update inc.php

--- inc.php
+++ inc.php
@@ -2,7 +2,7 @@
 // Connexion à la base de données
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=mini_chat_marine;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host='.(getenv('MYSQL_HOST') ?: 'localhost').';dbname=mini_chat_marine;charset=utf8', 'root', '');
 }
 catch(Exception $e)
 {
